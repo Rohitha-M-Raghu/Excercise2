73a3cacde95189c893dda19dcf45fe31a905378b
Rohitha-M-Raghu <rohitha13raghu00@gmail.com>
2023-02-23 18:31:22 +0530

    some minor edits

--- src/SampleOops.java
+++ src/SampleOops.java
@@ -3,8 +3,9 @@ import java.util.HashSet;
 //Browser Class
 class Browser {
 	Browser() {
-		System.out.println("Browser Object Created... ");
+//		System.out.println("Browser Object Created... ");
 	}
+
 	public void whoAmI() {
 		System.out.println("I am a browser");
 	}
@@ -13,6 +14,7 @@ class Browser {
 //MultipleAccountnewContainer interface
 interface MultipleAccountnewContainer {
 	public void addnewContainer(String newContainer);
+
 	public void leavenewContainer(String remContainer);
 }
 
@@ -22,46 +24,41 @@ class GoogleChrome extends Browser {
 	private static boolean isLocationAccessible;
 	private static boolean isCameraAccessible;
 	private static boolean isMicrophoneAccessible;
+
 	GoogleChrome() {
 		super();
 	}
-	
+
 	@Override
-	public void whoAmI() {		
+	public void whoAmI() {
 		System.out.println("I am GoogleChrome");
 	}
-	
-	private static void accessProvider(AccessAllowed key) {
-		if(key.equals(AccessAllowed.LOCATION)) {
-			isLocationAccessible = true;
-			System.out.println("Location Accessing...");
+
+
+	public static void setPermission() {
+		GoogleChrome.isCameraAccessible = true;
+		GoogleChrome.isLocationAccessible = true;
+		GoogleChrome.isMicrophoneAccessible = true;
+		System.out.println("Accessing Camera...");
+		System.out.println("Accessing Location...");
+		System.out.println("Accessing Microphone...");
+	}
+
+	public static void setPermission(boolean camera, boolean location, boolean microphone) {
+		GoogleChrome.isCameraAccessible = camera;
+		GoogleChrome.isLocationAccessible = location;
+		GoogleChrome.isMicrophoneAccessible = microphone;
+		if(GoogleChrome.isCameraAccessible = true) {
+			System.out.println("Accessing Camera...");
 		}
-		else if(key.equals(AccessAllowed.CAMERA)) {
-			isCameraAccessible = true;
-			System.out.println("Camera Accessing...");
+		else if(GoogleChrome.isLocationAccessible = true) {
+			System.out.println("Accessing Location...");
 		}
-		else {
-			isMicrophoneAccessible = true;
-			System.out.println("Microphone Accessing...");
+		else if(GoogleChrome.isMicrophoneAccessible = true) {
+			System.out.println("Accessing Microphone...");
 		}
-		
 	}
-	//Overloading
-	public static void setPermission(AccessAllowed key1) {
-		accessProvider(key1);
-	}
-	
-	public static void setPermission(AccessAllowed key1, AccessAllowed key2) {
-		accessProvider(key1);
-		accessProvider(key2);
-	}
-	
-	public static void setPermission(AccessAllowed key1, AccessAllowed key2, AccessAllowed key3) {
-		accessProvider(key1);
-		accessProvider(key2);
-		accessProvider(key3);
-	}
-	
+
 	public static void resetPermission() {
 		GoogleChrome.isCameraAccessible = false;
 		GoogleChrome.isLocationAccessible = false;
@@ -73,31 +70,32 @@ class GoogleChrome extends Browser {
 //Firefox
 class Firefox extends Browser implements MultipleAccountnewContainer {
 	private static HashSet<String> containers = new HashSet<String>(5);
-	
+
 	Firefox() {
-		System.out.println("Firefox Object Created... ");
+		super();
+//		System.out.println("Firefox Object Created... ");
 	}
-	
+
 	@Override
-	public void whoAmI() {	
+	public void whoAmI() {
 		System.out.println("I am Firefox");
 	}
-	
+
+	@Override
 	public void addnewContainer(String newContainer) {
-		if(containers.contains(newContainer)) {
-			System.out.println(newContainer +" already exists");
-		}
-		else {
-			containers.add(newContainer);	
+		if (containers.contains(newContainer)) {
+			System.out.println(newContainer + " already exists");
+		} else {
+			containers.add(newContainer);
 		}
 	}
-	
+
+	@Override
 	public void leavenewContainer(String remContainer) {
-		//leave newContainer
-		if(!containers.contains(remContainer)) {
-			System.out.println(remContainer +" doesn't exist inside the container");
-		}
-		else {
+		// leave newContainer
+		if (!containers.contains(remContainer)) {
+			System.out.println(remContainer + " doesn't exist inside the container");
+		} else {
 			containers.remove(remContainer);
 		}
 	}
@@ -105,6 +103,7 @@ class Firefox extends Browser implements MultipleAccountnewContainer {
 
 public class SampleOops {
 	public static void main(String[] args) {
+//		
 		Browser tabOne = new GoogleChrome();
 		Browser tabTwo = new Firefox();
 		Browser tabThree = new Firefox();
@@ -116,38 +115,44 @@ public class SampleOops {
 		allBrowsers[2] = tabThree;
 		allBrowsers[3] = tabFour;
 		allBrowsers[4] = tabFive;
-		//Finding the number of Tabs of Firefox and Chrome
+		// Finding the number of Tabs of Firefox and Chrome
 		int firefoxTabs = 0;
 		int chromeTabs = 0;
-		
-		for(int i=0;i<5;++i) {
-			if(allBrowsers[i] instanceof Firefox) {
-				firefoxTabs ++;
-			}
-			else {
-				chromeTabs ++;
+
+		for (int i = 0; i < 5; ++i) {
+			if (allBrowsers[i] instanceof Firefox) {
+				firefoxTabs++;
+			} else {
+				chromeTabs++;
 			}
 		}
-		
+
 		System.out.println("Number of firefox Tabs: " + firefoxTabs);
 		System.out.println("Number of Chrome Tabs: " + chromeTabs);
-		
-		AccessAllowed camera = AccessAllowed.CAMERA;
-		AccessAllowed location = AccessAllowed.LOCATION;
-		AccessAllowed microphone = AccessAllowed.MICROPHONE;
-		
-		//Permission settings for Chrome
-		GoogleChrome.setPermission(microphone);
+
+		/*
+		 * AccessAllowed camera = AccessAllowed.CAMERA; AccessAllowed location =
+		 * AccessAllowed.LOCATION; AccessAllowed microphone = AccessAllowed.MICROPHONE;
+		 * 
+		 * //Permission settings for Chrome
+		 */
+		GoogleChrome.setPermission(true, false, false);
 		GoogleChrome.resetPermission();
-		GoogleChrome.setPermission(location, microphone);
+		GoogleChrome.setPermission(true, false, true);
 		GoogleChrome.resetPermission();
-		GoogleChrome.setPermission(camera, location, microphone);
-		
+		GoogleChrome.setPermission();
+
 		Firefox browserOfFirefox = (Firefox) tabTwo;
 		browserOfFirefox.addnewContainer("Messenger");
 		browserOfFirefox.addnewContainer("Mails");
-		
-		browserOfFirefox.leavenewContainer("Messenger");	
-	}
-} 
 
+		browserOfFirefox.leavenewContainer("Messenger");
+		Browser chrome1 = new GoogleChrome();
+		Browser firefox1 = new Firefox();
+		chrome1.whoAmI();
+		firefox1.whoAmI();
+		Browser browser = new Browser();
+		browser.whoAmI();
+
+	}
+}
